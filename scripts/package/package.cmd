8aa0b2828394fe0801f01b81ddabaa23b90d7346
Eric Erhardt <eric.erhardt@microsoft.com>
2016-02-16 13:32:53 -0600

    Fix PackageDir in package.cmd

--- scripts/package/package.cmd
+++ scripts/package/package.cmd
@@ -3,4 +3,6 @@
 REM Copyright (c) .NET Foundation and contributors. All rights reserved.
 REM Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+set PackageDir="%~dp0..\..\artifacts\packages\dnvm"
+
 powershell -NoProfile -NoLogo -Command "%~dp0package.ps1 %*; exit $LastExitCode;"
